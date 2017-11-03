91e78973c366384cd0d03611c1a3daa44d9fff19
zhughes3 <zhughes3@gmail.com>
2017-11-03 19:42:05 -0400

    adjusted http routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Blog from '../components'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
+  mode: 'history'
+  , linkActiveClass: 'active'
+  , routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      path: '/'
+      , name: 'feed'
+      , component: Blog
+    }
+    , {
+      path: '/by/:author'
+      , name: 'author'
+      , props: true
+      , component: Blog
+    }
+    , {
+      path: '/read/:post'
+      , name: 'post'
+      , props: true
+      , component: Blog
     }
   ]
 })
